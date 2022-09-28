a7772418f824fcb0184e08da43ff00645f8a460c
Odelon Jhey Pacalso <odjhey@gmail.com>
2022-09-28 14:16:16 +0800

    update deployment config

--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/shuffle-and-draw/webapp/dist/",
+  base: "/shuffle-and-draw/",
   plugins: [react()],
 });
